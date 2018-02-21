6869ad869472de88c2d1ac2d659fefc53c4e2363
Devesh <devsrawt@gmail.com>
2018-02-21 18:31:02 +0530

    After Currrent Jobs

--- models/admitcard.js
+++ models/admitcard.js
@@ -3,6 +3,7 @@ var admitSchema = new mongoose.Schema({
     title: String,
     category: String,
     created: { type: Date, default: Date.now },
+    postDate: String,
     download: String
 });
 module.exports = mongoose.model('Admit', admitSchema);
